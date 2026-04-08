a4bc6a803d74ff3562d499934d83b3fa89e65057
Animesh Singh <animeshsinghbki2282@gmail.com>
2026-04-09 05:09:36 +0530

    perf: replace useAtom with useSetAtom in Buttons to prevent unnecessary re-renders

--- w-7/jotai/src/App.jsx
+++ w-7/jotai/src/App.jsx
@@ -1,7 +1,13 @@
-import { useAtom, useAtomValue } from "jotai";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { countAtom } from "./store/atoms/count";
 
 function App() {
+  return <Count />;
+}
+
+function Count() {
+  console.log("Count reRendered");
+
   return (
     <div>
       <CountRender />
@@ -16,11 +22,13 @@ function CountRender() {
 }
 
 function Buttons() {
-  const [count, setCount] = useAtom(countAtom);
+  const setCount = useSetAtom(countAtom);
+  console.log("Button reRender");
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount((c) => c + 1)}>Increase</button>
+      <button onClick={() => setCount((c) => c - 1)}>Decrease</button>
     </div>
   );
 }
